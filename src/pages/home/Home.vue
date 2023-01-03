994dc6934f8b3114aa0f510a8da199241b9f4633
mazhengwei <muyan@192.168.0.103>
2023-01-03 23:36:24 +0800

    添加图标区域

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,19 +2,21 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
-    <div>test</div>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from '@/pages/home/components/Swiper.vue'
+import HomeIcons from './components/Icons.vue'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Home',
   components: {
     HomeSwiper,
-    HomeHeader
+    HomeHeader,
+    HomeIcons
   }
 }
 </script>
---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png"/>
+      </div>
+      <p class="icon-desc">热门景点</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/assets/styles/variable.styl"
+  .icons
+    overflow hidden
+    height 0
+    padding-bottom 50%
+    .icon
+      position relative
+      overflow hidden
+      float left
+      height 0
+      width 25%
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        text-align center
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+</style>
